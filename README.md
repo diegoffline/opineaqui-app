d0bb3083f5dccd3aa843a4b08c56810e645c7116
Diego Marchetti <diego.marchetti@fatec.sp.gov.br>
2016-11-04 01:28:24 -0200

    incio do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# opineaqui-app
---
+++ app/src/main/java/com/priced/opine/ListProduct.java
@@ -0,0 +1,26 @@
+package com.priced.opine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by diego on 28/10/16.
+ */
+
+public class ListProduct extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_product, container, false);
+    return  view;
+    }
+
+
+
+
+}
